a2c56838e311faa4d1e343e4811d9170dbee9c10
Ahmed Ghonem <56454082+3ba2ii@users.noreply.github.com>
2019-12-18 17:36:46 +0200

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,14 +1,16 @@
 from sly import Lexer
 
 class MyLexer(Lexer):
-    #Every token our programming languages understands 
+    #Every Terminals our programming languages understands 
     tokens = { VARIABLE_NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUNC, TO, ARROW, EQUALEQUAL }
+
+    #Ignores Taps and Spaces
     ignore = '\t '
     #operators and others
-    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';' }
+    literals = { '=', '+', '-', '/', '*',')','(' }
     
 
-    #Language Grammer
+
     """The r means that the string is to be treated as a raw string, which means all escape codes will be ignored.
 
     For an example:
@@ -18,7 +20,7 @@ class MyLexer(Lexer):
     """
     
     """
-    [0-9]	Returns a match for any digit between 0 and 9	Try it »
+    [0-9]	Returns a match for any digit between 0 and 9	
     [0-5][0-9]	Returns a match for any two-digit numbers from 00 and 59
     [a-zA-Z]	Returns a match for any character alphabetically between a and z, lower case OR upper case
     []	A set of characters	"[a-m]"
@@ -27,7 +29,7 @@ class MyLexer(Lexer):
     .	Any character (except newline character)
     check "https://www.w3schools.com/python/python_regex.asp" for more 
     """
-    
+    #Our Tokens 
     IF = r'IF'
     THEN=r'THEN'
     ELSE=r'ELSE'
@@ -37,11 +39,11 @@ class MyLexer(Lexer):
     ARROW=r'->'
     VARIABLE_NAME=r'[a-zA-Z_][a-zA-Z0-9]*'  #Variable VARIABLE_NAMEs must start with (a~z or A~Z or _) then anything in the second character
 
-    STRING=r'".*?"'         #.* => zero or more occurances of any charachter except new line   
+    STRING=r'"[a-zA-Z0-9_=+-]*"'         #.*? => zero or more occurances of any charachter except new line   
     EQUALEQUAL=r'=='
 
 
-    """To make control of number tokens"""
+    #To make control of number tokens
 
     #\d+ means it has to be one or more digits
     @_(r'\d+')
@@ -53,9 +55,7 @@ class MyLexer(Lexer):
     @_(r'#.*')
     def COMMENT(self,comm):
         pass
-    @_(r'\n+')
-    def NEWLINE(self,t):
-        self.lineno=t.value.count('\n')
+
 
 
 """
@@ -64,7 +64,7 @@ What does actually the parser do ?
     -Check if this pattern actually means anything
     -example:
     -FUNC VARIABLE_NAME() -> a+b;
-    -means if founc -> after the FUNC return this operation
+    -means if found -> after the FUNC return this operation
     -The Parser has to figure out if the language matches our grammar  
 """
 
